f78417d31ada66c10782d80850009d0fda0150b9
adiXM <gemene_adrian@yahoo.com>
2022-12-23 01:04:43 +0200

    Create sample homepage

--- app/src/Controller/HomepageController.php
+++ app/src/Controller/HomepageController.php
@@ -11,7 +11,7 @@ class HomepageController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
-        return $this->render('homepage/index.html.twig', [
+        return $this->render('pages/homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
     }
