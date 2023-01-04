ae57f0ee115928d80d23d0d9f459e12d1b4dcb92
adiXM <gemene_adrian@yahoo.com>
2023-01-05 00:07:12 +0200

    Add DeleteFormType

---
+++ app/src/Form/DeleteFormType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DeleteFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('roomId', HiddenType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'class' => 'hidden',
+                ],
+            ])
+        ;
+
+    }
+}
