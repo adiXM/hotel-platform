87a78e64f2658b42ba08d30fc9ad8965f939855c
adiXM <gemene_adrian@yahoo.com>
2023-01-12 23:53:57 +0200

    Move price field room to room type

---
+++ app/migrations/Version20230111183224.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230111183224 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE room DROP price');
+        $this->addSql('ALTER TABLE room_type ADD price DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE room_type DROP price');
+        $this->addSql('ALTER TABLE room ADD price DOUBLE PRECISION NOT NULL');
+    }
+}
